0a50b6461969dbd3d219f441426f010891279fe9
Julia Zolotarev <jzolotarev@gmail.com>
2021-03-17 13:49:00 -0400

    add tests for increment, decrement, reset counter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,43 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+// import { render } from '@testing-library/dom'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the correct content', () => {
+  // render(<App />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+  // const root = document.createElement("div")
+  // ReactDOM.render(<App />, root)
+  
+  const { getByText, getByLabelText } = render(<App />)
+
+  getByText("Counter") // same as line below
+  // expect(getByText("Counter")).not.toBeNull()
 });
+
+test("allows user to increment counter", () => {
+  const { getByText, getByTestId } = render(<App />)
+
+  fireEvent.click(getByText("Click me to Increment"))
+  getByText(1)
+
+})
+
+test("allows user to decrement counter", () => {
+  const { getByText, getByTestId } = render(<App />)
+
+  fireEvent.click(getByText("Click me to Decrement"))
+  getByText(-1)
+
+})
+
+test("allows user to reset counter", () => {
+  const { getByText, getByTestId } = render(<App />)
+
+  fireEvent.click(getByText("Click me to Reset"))
+  getByText(0)
+
+})
+
+
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div>
       <h1>Counter</h1>
+      <h3 id="count">{count}</h3>
+      <button onClick={(() => setCount(count + 1))}>Click me to Increment</button>
+      <button onClick={(() => setCount(count - 1))}>Click me to Decrement</button>
+      <button onClick={(() => setCount(0))}>Click me to Reset</button>
     </div>
   );
 }
